a3b6c33c191529ac8975df7d35a87933ad730614
Denis <like-nix@mail.ru>
2024-12-09 23:07:44 +0300

    add i18nexample

---
+++ i18n/i18next/basic-example/src/App.tsx
@@ -0,0 +1,64 @@
+import { Component, Suspense } from 'react';
+import { useTranslation, withTranslation, Trans } from 'react-i18next';
+import './App.css';
+
+// use hoc for class based components
+class LegacyWelcomeClass extends Component {
+  render() {
+    const { t } = this.props;
+    return <h2>{t('title')}</h2>;
+  }
+}
+const Welcome = withTranslation('common')(LegacyWelcomeClass);
+
+// Component using the Trans component
+function MyComponent() {
+  return (
+    <Trans i18nKey="description.part1">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </Trans>
+  );
+}
+
+// page uses the hook
+function Page() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: string) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <Welcome />
+        <button type="button" onClick={() => changeLanguage('ru')}>
+          ru
+        </button>
+        <button type="button" onClick={() => changeLanguage('en')}>
+          en
+        </button>
+      </div>
+      <div className="App-intro">
+        <MyComponent />
+      </div>
+      <div>{t('description.part2')}</div>
+    </div>
+  );
+}
+
+// loading component for suspense fallback
+const Loader = () => (
+  <div className="App">
+    <div>loading...</div>
+  </div>
+);
+
+// here app catches the suspense from page in case translations are not yet loaded
+export default function App() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Page />
+    </Suspense>
+  );
+}
